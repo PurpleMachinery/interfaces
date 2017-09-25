75d72fb2d12881f56ab2005f6e7fe622b4af5a5e
wesley de vieira lima <wesley.vieira.lima.1999@gmail.com>
2017-09-25 05:04:02 -0300

    Botao Salvar, Imprimir e Limpar configurados

--- src/model/PessoaFisica.java
+++ src/model/PessoaFisica.java
@@ -8,84 +8,84 @@ public class PessoaFisica {
 		return nome;
 	}
 
-	public String getEndereco() {
-		return endereco;
-	}
-
-	public String getBairro() {
-		return bairro;
-	}
-
-	public String getCep() {
-		return cep;
-	}
-
-	public String getCidade() {
-		return cidade;
-	}
-
-	public String getEstado() {
-		return estado;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public String getCelular() {
-		return celular;
-	}
-
-	public String getRg() {
-		return rg;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public char getSexo() {
-		return sexo;
-	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
 	}
 
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
 
+	public String getBairro() {
+		return bairro;
+	}
+
 	public void setBairro(String bairro) {
 		this.bairro = bairro;
+	}
+
+	public String getCep() {
+		return cep;
 	}
 
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
 
+	public String getCidade() {
+		return cidade;
+	}
+
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
 	}
 
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
 
+	public String getTelefone() {
+		return telefone;
+	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
+	}
+
+	public String getCelular() {
+		return celular;
 	}
 
 	public void setCelular(String celular) {
 		this.celular = celular;
 	}
 
+	public String getRg() {
+		return rg;
+	}
+
 	public void setRg(String rg) {
 		this.rg = rg;
 	}
 
+	public String getCpf() {
+		return cpf;
+	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
+	}
+
+	public char getSexo() {
+		return sexo;
 	}
 
 	public void setSexo(char sexo) {
