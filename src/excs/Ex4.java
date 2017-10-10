b7bfcfb0666523688a300c49f94b3fe291e1dfc2
Wesley V. Lima <wesley.vieira.lima.1999@gmail.com>
2017-10-10 15:54:36 -0300

    Exercicio 4 Pronto

---
+++ src/excs/Ex4.java
@@ -0,0 +1,50 @@
+package excs;
+
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class Ex4 extends JFrame{
+	JTextField txb0 = new JTextField();
+	JTextField txb1 = new JTextField();
+	JTextField txb2 = new JTextField();
+	JTextField txb3 = new JTextField();
+	
+	JButton btn = new JButton("Calcular");
+	
+	
+	public Ex4() {
+		Container paine = this.getContentPane();
+		paine.setLayout(null);
+		this.setSize(500,500);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+
+		paine.add(txb0);
+		txb0.setBounds(0,0,485,40);
+		paine.add(txb1);
+		txb1.setBounds(0,40,485,40);
+		paine.add(txb2);
+		txb2.setBounds(0,80,485,40);
+		paine.add(btn);
+		btn.setBounds(0, 120, 485, 40);
+		paine.add(txb3);
+		txb3.setBounds(0,160,485,40);
+		txb3.setEditable(false);
+		btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txb3.setText(""+(/**/(Double.parseDouble(txb0.getText())+Double.parseDouble(txb1.getText())+Double.parseDouble(txb2.getText()))/3)/**/);
+			}
+		});
+		
+	}
+	
+	public static void main(String args[]) {
+		Ex4 ex = new Ex4();
+	}
+}
